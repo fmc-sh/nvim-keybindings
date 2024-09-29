595ee4c2f47d5b7c9ddb79049aca59f732fc4fab
f-h1 <154448936+f-h1@users.noreply.github.com>
2024-09-29 14:09:35 +0200

    update

--- lua/nvim-keybindings/init.lua
+++ lua/nvim-keybindings/init.lua
@@ -17,7 +17,22 @@ function M.setup() end
 -- )
 
 -- Keybinding to toggle the same terminal session later
-vim.api.nvim_set_keymap("n", "<A-i>", "<CMD>lua toggle_my_term()<CR>", { noremap = true, silent = true })
+--vim.api.nvim_set_keymap("n", "<A-i>", "<CMD>lua toggle_my_term()<CR>", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("t", "<A-i>", "<C-\\><C-n><CMD>lua toggle_my_term()<CR>", { noremap = true, silent = true })
+--vim.api.nvim_set_keymap("t", "<A-i>", "<C-\\><C-n><CMD>lua toggle_my_term()<CR>", { noremap = true, silent = true })
+
+-- Set keybinding to toggle the terminal
+vim.api.nvim_set_keymap(
+	"n",
+	"<A-i>",
+	"<CMD>lua require('nvim-fterm-init').toggle_my_term()<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"t",
+	"<A-i>",
+	"<C-\\><C-n><CMD>lua require('nvim-fterm-init').toggle_my_term()<CR>",
+	{ noremap = true, silent = true }
+)
+
 return M
