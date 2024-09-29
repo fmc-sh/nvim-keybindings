4708946d1a3f7ea84ad93982f0ceeedef6fc0e08
f-h1 <154448936+f-h1@users.noreply.github.com>
2024-09-29 15:06:23 +0200

    u

--- lua/nvim-keybindings/init.lua
+++ lua/nvim-keybindings/init.lua
@@ -35,4 +35,11 @@ vim.api.nvim_set_keymap(
 	{ noremap = true, silent = true }
 )
 
+--------------------------------------
+-- FTerm
+--------------------------------------
+
+vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true, silent = true })
+
 return M
