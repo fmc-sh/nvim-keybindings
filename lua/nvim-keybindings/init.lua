a78c73ca9699910c6fd81471bc3885993846035c
f-h1 <154448936+f-h1@users.noreply.github.com>
2024-10-02 11:20:15 +0200

    telescope

--- lua/nvim-keybindings/init.lua
+++ lua/nvim-keybindings/init.lua
@@ -51,52 +51,65 @@ vim.api.nvim_set_keymap("t", "<A-j>", "<C-\\><C-n>", { noremap = true, silent = 
 --------------------------------------
 -- Vim-obsession
 --------------------------------------
-
--- Assuming you already have Telescope and vim-obsession installed
-
--- Define your session directory
-local session_dir = vim.fn.expand("~/.vim-sessions")
-
--- Function to save the current session
-function SaveObsessionSession()
-	local session_name = vim.fn.substitute(vim.fn.getcwd(), "/", "_", "g") -- Replace slashes with underscores
-	local session_path = session_dir .. "/" .. session_name
-	vim.cmd("Obsess " .. session_path)
-	print("Session saved to " .. session_path)
-end
-
--- Function to load a selected session from Telescope
-function LoadObsessionSession(session)
-	local session_path = session_dir .. "/" .. session
-	vim.cmd("source " .. session_path)
-	print("Session loaded from " .. session_path)
-end
-
--- Key mapping to save the session
-vim.api.nvim_set_keymap("n", "<leader>vs", ":lua SaveObsessionSession()<CR>", { noremap = true, silent = true })
-
--- Use Telescope to list sessions in the session directory and load the selected session
-require("telescope").setup({})
-
-local actions = require("telescope.actions")
-
-function BrowseSessions()
-	require("telescope.builtin").find_files({
-		prompt_title = "< Browse Sessions >",
-		cwd = session_dir,
-		attach_mappings = function(prompt_bufnr, map)
-			-- On selection, source the selected session
-			map("i", "<CR>", function()
-				local selection = require("telescope.actions.state").get_selected_entry()
-				actions.close(prompt_bufnr)
-				LoadObsessionSession(selection.value)
-			end)
-			return true
-		end,
-	})
-end
-
--- Key mapping to browse and load sessions with Telescope
-vim.api.nvim_set_keymap("n", "<leader>vb", ":lua BrowseSessions()<CR>", { noremap = true, silent = true })
-
+-- -- These are obsolete now... use session dashboard
+-- --
+-- -- Assuming you already have Telescope and vim-obsession installed
+--
+-- -- Define your session directory
+-- local session_dir = vim.fn.expand("~/.vim-sessions")
+--
+-- -- Function to save the current session
+-- function SaveObsessionSession()
+-- 	local session_name = vim.fn.substitute(vim.fn.getcwd(), "/", "_", "g") -- Replace slashes with underscores
+-- 	local session_path = session_dir .. "/" .. session_name
+-- 	vim.cmd("Obsess " .. session_path)
+-- 	print("Session saved to " .. session_path)
+-- end
+--
+-- -- Function to load a selected session from Telescope
+-- function LoadObsessionSession(session)
+-- 	local session_path = session_dir .. "/" .. session
+-- 	vim.cmd("source " .. session_path)
+-- 	print("Session loaded from " .. session_path)
+-- end
+--
+-- -- Key mapping to save the session
+-- vim.api.nvim_set_keymap("n", "<leader>vs", ":lua SaveObsessionSession()<CR>", { noremap = true, silent = true })
+--
+-- -- Use Telescope to list sessions in the session directory and load the selected session
+-- require("telescope").setup({})
+--
+-- local actions = require("telescope.actions")
+--
+-- function BrowseSessions()
+-- 	require("telescope.builtin").find_files({
+-- 		prompt_title = "< Browse Sessions >",
+-- 		cwd = session_dir,
+-- 		attach_mappings = function(prompt_bufnr, map)
+-- 			-- On selection, source the selected session
+-- 			map("i", "<CR>", function()
+-- 				local selection = require("telescope.actions.state").get_selected_entry()
+-- 				actions.close(prompt_bufnr)
+-- 				LoadObsessionSession(selection.value)
+-- 			end)
+-- 			return true
+-- 		end,
+-- 	})
+-- end
+--
+-- -- Key mapping to browse and load sessions with Telescope
+-- vim.api.nvim_set_keymap("n", "<leader>vb", ":lua BrowseSessions()<CR>", { noremap = true, silent = true })
+--
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>ff",
+	'<cmd>lua require("telescope.builtin").find_files()<CR>',
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>ft",
+	'<cmd>lua require("telescope.builtin").live_grep()<CR>',
+	{ noremap = true, silent = true }
+)
 return M
